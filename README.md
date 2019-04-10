59c9fb8d0ea0027b01870e473a76fa8a42189214
makinoshi <masa.kinoshita02@gmail.com>
2019-04-10 11:09:11 +0900

    Add fish config

--- README.md
+++ README.md
@@ -9,3 +9,20 @@ Run this:
 ```shell
 $ ln -s <full path of .spacemacs> ~/
 ```
+
+
+## config.fish
+
+My [fish](https://fishshell.com/) configuration.
+
+Run this:
+
+```shell
+$ ln -s <full path of my_config.fish> ~/.config/fish/
+```
+
+Write following in `~/.config/fish/config.fish`
+
+```fish
+source ~/.config/fish/my_config.fish
+```
---
+++ my_config.fish
@@ -0,0 +1,28 @@
+# -------------------------------------
+# aliases
+# -------------------------------------
+# ls
+alias l 'ls -lha'
+# git
+alias gs 'git status'
+alias gcb 'git checkout -b'
+alias gc 'git checkout'
+alias gcd 'git checkout develop'
+alias grd 'git rebase develop'
+alias gf 'git fetch -p'
+alias gd 'git diff'
+alias ga 'git branch -a'
+alias gp 'git pull'
+alias gb 'git branch'
+alias gbv 'git branch -vv'
+alias gl 'git log --graph'
+# cd
+alias pj 'cd ~/projects'
+alias ... 'cd ../..'
+alias .... 'cd ../../..'
+# for safe
+alias cp 'cp -i'
+alias mv 'mv -i'
+alias rm 'rm -i'
+# Mac
+alias c 'pbcopy'
