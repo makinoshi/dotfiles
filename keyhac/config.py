b9b9d7292cbacc2e4e4bc6eb5c3901f8ac8e54a7
makinoshi <masa.kinoshita02@gmail.com>
2024-01-07 22:52:27 +0900

    Change to use CapsLock as a F13 key

--- keyhac/config.py
+++ keyhac/config.py
@@ -23,94 +23,80 @@ def configure(keymap):
     # --------------------------------------------------------------------
     # Key remap
 
-    # 設定値
-    isJISKeyboard = False  # JISキーボードかどうか
-    modifiers = []  # モディファイアのリスト
-
-    # 無変換と変換をモディファイアにする
-    if isJISKeyboard:
-        keymap.replaceKey("(29)", 235)  # 無変換
-        keymap.replaceKey("(28)", 236)  # 変換
-        keymap.defineModifier(235, "User0")
-        keymap.defineModifier(236, "User1")
-        modifiers = ["U0-", "U1-"]
-    else:
-        keymap.replaceKey("Space", 235)
-        keymap.defineModifier(235, "User0")
-        modifiers = ["U0-"]
-
     # Global keymap which affects any windows
     keymap_global = keymap.defineWindowKeymap()
 
-    # モディファイアにしたキーにワンショットモディファイアを割り当てる
-    if isJISKeyboard:
-        # 無変換と変換をワンショットモディファイアでIMEの有効無効を切り替える
-        keymap_global["O-(235)"] = lambda: keymap.wnd.setImeStatus(0)
-        keymap_global["O-(236)"] = lambda: keymap.wnd.setImeStatus(1)
-    else:
-        # Spaceをワンショットモディファイアでスペースに戻す
-        for modifier_prefix in (
-            "",
-            "S-",
-            "C-",
-            "C-S-",
-            "A-",
-            "A-S-",
-            "A-C-",
-            "A-C-S-",
-        ):
-            keymap_global["O-" + modifier_prefix + "(235)"] = modifier_prefix + "Space"
+    # 設定値
+    isJISKeyboard = False  # JISキーボードかどうか
+
+    # F13(0x7c)をモディファイアとして使う
+    keymap.defineModifier("(124)", "User0")
+
+    # スペースバーからCtrl, Alt, Winとなるようにする
+    keymap.replaceKey("LAlt", "LCtrl")
+    keymap.replaceKey("LWin", "LAlt")
+    keymap.replaceKey("LCtrl", "LWin")
+    keymap.replaceKey("RAlt", "RCtrl")
+    keymap.replaceKey("RWin", "RAlt")
+    keymap.replaceKey("RCtrl", "RWin")
+
+    # スペースバーの隣のキーでIMEの有効無効を切り替える
+    keymap_global["O-LCtrl"] = lambda: keymap.wnd.setImeStatus(0)
+    keymap_global["O-RCtrl"] = lambda: keymap.wnd.setImeStatus(1)
+
+    # カスタマイズ用のファンクションキー
+    keymap_global["U0-Space"] = "(125)" # F14(VSCodeではVSpaceCode)
+    keymap_global["U0-Comma"] = "(126)" # F15(VSCodeではVSpaceCodeのメジャーモードを起動)
 
     # 移動
-    for modifier in modifiers:
-        for modifier_prefix in (
-            "",
-            "S-",
-            "C-",
-            "C-S-",
-            "A-",
-            "A-S-",
-            "A-C-",
-            "A-C-S-",
-            "W-",
-            "W-S-",
-            "W-C-",
-            "W-C-S-",
-            "W-A-",
-            "W-A-S-",
-            "W-A-C-",
-            "W-A-C-S-",
-        ):
-            for k, v in {
-                # 矢印キー
-                "J": "Left",
-                "K": "Down",
-                "I": "Up",
-                "L": "Right",
-                # Home/End
-                "A": "Home",
-                "E": "End",
-                # PageUp/PageDown
-                "U": "PageUp",
-                "O": "PageDown",
-            }.items():
-                keymap_global[modifier_prefix + modifier + k] = modifier_prefix + v
+    for modifier_prefix in (
+        "",
+        "S-",
+        "C-",
+        "C-S-",
+        "A-",
+        "A-S-",
+        "A-C-",
+        "A-C-S-",
+        "W-",
+        "W-S-",
+        "W-C-",
+        "W-C-S-",
+        "W-A-",
+        "W-A-S-"
+        "W-A-C-",
+        "W-A-C-S-",
+    ):
+        for key, key_name in {
+            # 矢印キー
+            "J": "Left",
+            "K": "Down",
+            "I": "Up",
+            "L": "Right",
+            # Home/End
+            "A": "Home",
+            "E": "End",
+            # PageUp/PageDown
+            "U": "PageUp",
+            "O": "PageDown",
+        }.items():
+            keymap_global[modifier_prefix + "U0-" + key] = modifier_prefix + key_name
 
     # 単語移動
-    for modifier in modifiers:
-        for modifier_prefix in ("", "S-"):
-            keymap_global[modifier_prefix + modifier + "W"] = (
-                modifier_prefix + "C-Right"
-            )
-            keymap_global[modifier_prefix + modifier + "B"] = modifier_prefix + "C-Left"
+    for modifier_prefix in ("", "S-"):
+        keymap_global[modifier_prefix + "U0-W"] = (
+            modifier_prefix + "C-Right"
+        )
+        keymap_global[modifier_prefix + "U0-B"] = modifier_prefix + "C-Left"
 
     # Backspace
     keymap_global["U0-H"] = "Back"
-    keymap_global["U0-LC-H"] = "LC-Back"
+    keymap_global["U0-C-H"] = "C-Back"
 
     # Delete
     keymap_global["U0-X"] = "Delete"
-    keymap_global["U0-LS-D"] = "S-End", "C-X"
+    keymap_global["U0-C-X"] = "C-Delete"
+    keymap_global["U0-S-D"] = "S-End", "C-X"
     keymap_global["U0-D"] = keymap.defineMultiStrokeKeymap("d: kill line, w: kill word")
     keymap_global["U0-D"]["U0-D"] = "Home", "Home", "S-Down", "C-x"
     keymap_global["U0-D"]["U0-W"] = "C-S-Right", "C-x"
@@ -119,19 +105,39 @@ def configure(keymap):
     keymap_global["U0-Z"] = "C-Z"
     keymap_global["U0-Y"] = "C-Y"
 
-    # LCtrlのワンショットモディファイアでF21を送信する。VSCodeではwhichkeyが起動する
-    keymap_global["O-LCtrl"] = "(132)"
-
     # ESC
     if isJISKeyboard:
         keymap_global["U0-Plus"] = "ESC"
     else:
         keymap_global["U0-Semicolon"] = "ESC"
 
-    # Space
-    if not isJISKeyboard:
-        keymap_global["U0-S"] = "Space"
-        keymap_global["U0-S-S"] = "S-Space"
+    # Function keys
+    for modifier_prefix in (
+        "",
+        "S-",
+        "C-",
+        "C-S-",
+        "A-",
+        "A-S-",
+        "A-C-",
+        "A-C-S-",
+    ):
+        keymap_global[modifier_prefix + "U0-1"] = modifier_prefix + "F1"
+        keymap_global[modifier_prefix + "U0-2"] = modifier_prefix + "F2"
+        keymap_global[modifier_prefix + "U0-3"] = modifier_prefix + "F3"
+        keymap_global[modifier_prefix + "U0-4"] = modifier_prefix + "F4"
+        keymap_global[modifier_prefix + "U0-5"] = modifier_prefix + "F5"
+        keymap_global[modifier_prefix + "U0-6"] = modifier_prefix + "F6"
+        keymap_global[modifier_prefix + "U0-7"] = modifier_prefix + "F7"
+        keymap_global[modifier_prefix + "U0-8"] = modifier_prefix + "F8"
+        keymap_global[modifier_prefix + "U0-9"] = modifier_prefix + "F9"
+        keymap_global[modifier_prefix + "U0-0"] = modifier_prefix + "F10"
+        keymap_global[modifier_prefix + "U0-Minus"] = modifier_prefix + "F11"
+        keymap_global[modifier_prefix + "U0-Plus"] = modifier_prefix + "F12"
+
+    # Ctrlとして動作させる
+    keymap_global["U0-Tab"] = "C-Tab"
+    keymap_global["U0-S-Tab"] = "C-S-Tab"
 
     # Clipboard history related
     keymap_global[
